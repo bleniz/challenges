14878f7475f93b3c86dafb7e397e39a033a83dc5
Zac Bleninger <zac.bleninger@thomsonreuters.com>
2017-08-25 13:07:20 -0500

    Adding HealthBarChallenge class, no work done, solution added later

---
+++ main/other/HealthBarChallenge.java
@@ -0,0 +1,15 @@
+public class HealthBarChallenge
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public int calculateMinimumHealth(int[] points)
+    {
+        int minimumHealth = 1;
+
+        return minimumHealth;
+    }
+}
+
